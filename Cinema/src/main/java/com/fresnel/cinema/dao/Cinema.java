8ababb0f46f9930c4dc07126b352750a9b3c3e2b
billfres <ngomafresnel@gmail.com>
2022-01-09 00:59:38 +0100

    Initialisation du projzt avec la cleation d'une classe cinema

---
+++ Cinema/src/main/java/com/fresnel/cinema/dao/Cinema.java
@@ -0,0 +1,18 @@
+package com.fresnel.cinema.dao;
+
+import javax.annotation.Generated;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+//import java.io.Serializable;
+
+@Entity
+@Data
+public class Cinema /*implements Serializable*/{
+	
+
+}
