de164b1b00e4dcfdabd0698d31715207225494c7
billfres <ngomafresnel@gmail.com>
2022-01-31 04:37:48 +0100

    Ajout de tous les services pour initialiser les données

--- Cinema/src/main/java/com/fresnel/cinema/service/ICinemaInitService.java
+++ Cinema/src/main/java/com/fresnel/cinema/service/ICinemaInitService.java
@@ -4,10 +4,10 @@ public interface ICinemaInitService {
 	 	public void initVilles();
 	    public void initCinemas();
 	    public void initSalles();
-	    public void initSeances();
 	    public void initPlaces();
+	    public void initSeances();
+	    public void initCategories();
 	    public void initFilms();
 	    public void initProjections();
 	    public void initTickets();
-	    public void initCategories();
 }
